624d2c88ae5fcacc6bfbf472da833f502bfa6550
OlegLobantsev <j.legis@gmail.com>
2023-03-14 11:21:02 +0300

    init

---
+++ src/js/__tests__/test.js
@@ -0,0 +1,33 @@
+import Team from '../app';
+
+class Character {
+  constructor(name, role) {
+    this.name = name;
+    this.role = role;
+  }
+}
+
+const hero0 = new Character('J0', 'Zombie');
+const hero1 = new Character('J1', 'Zombie');
+const hero2 = new Character('J2', 'Zombie');
+const hero3 = hero0;
+
+test('add', () => {
+  const comandos = new Team();
+  comandos.add(hero0);
+  expect(() => {
+    comandos.add(hero3);
+  }).toThrow(Error);
+});
+
+test('addAll', () => {
+  const comandos = new Team();
+  comandos.addAll(hero0, hero1);
+  expect(comandos.toArray()).toEqual([hero0, hero1]);
+});
+
+test('toArray', () => {
+  const comandos = new Team();
+  comandos.addAll(hero0, hero3, hero1, hero2);
+  expect(comandos.toArray()).toEqual([hero0, hero1, hero2]);
+});
